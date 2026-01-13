0c1c0577268152064686bbec2d3121b3e8711a91
lucaNeves <luizneves6@hotmail.com>
2026-01-13 17:19:53 -0300

    capitulo 07 adicionado

---
+++ cap07/main.tf
@@ -0,0 +1,46 @@
+resource "google_compute_address" "static_ip" {
+  name = "ipv4-address"
+}
+
+resource "google_compute_firewall" "allow_ssh_ex" {
+  name = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports = [ "22", "80" ]
+  }
+
+  source_ranges = [ "0.0.0.0/0" ]
+}
+
+resource "google_compute_instance" "vm" {
+  name = "vm-exemplo"
+  machine_type = "e2-micro"
+  zone = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet.id
+    access_config {
+      nat_ip = google_compute_address.static_ip.address
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+      #!/bin/bash
+
+      sudo apt update
+      sudo apt install nginx -y
+      sudo systemctl start nginx
+      sudo systemctl enable nginx
+    EOT
+  
+  depends_on = [ google_compute_address.static_ip ]
+}
